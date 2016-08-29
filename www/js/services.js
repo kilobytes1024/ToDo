8d8c135574502db2f7f93c707d398de448342576
kilobytes1024 <thobejanekabelo@gmail.com>
2016-08-29 16:20:56 +0200

    Updates

--- www/js/services.js
+++ www/js/services.js
@@ -2,15 +2,7 @@ angular.module('todo.services', [])
 
 
 .factory('toDos', function() {
-  // Might use a resource here that returns a JSON array
   
-  // Some fake testing data
-  //var test = [{content:'Store HTML 5', createdOn:Date()}];
-  //localStorage.setItem('toDos', JSON.stringify(test));
-
-//Test
-//localStorage.removeItem('toDos');
-
   var toDos = localStorage.getItem("toDos")? JSON.parse(localStorage.getItem("toDos")): [];
   
   console.log(toDos);
@@ -22,14 +14,6 @@ angular.module('todo.services', [])
     complete: function(toDo) {
 
       toDo.completedOn = Date();
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (toDos[i].id === parseInt(toDos)) {
-          return toDos[i];
-        }
-      }
-      return null;
     },
     persist: function(){
        localStorage.setItem('toDos', JSON.stringify(toDos));
